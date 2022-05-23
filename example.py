e2fffdaf5cdd3a5f6c3642463a44518014922a10
Kasper Kouhia <60173278+kasperkouhia@users.noreply.github.com>
2022-05-23 21:01:01 +0300

    Randomized example matrix

--- example.py
+++ example.py
@@ -1,5 +1,12 @@
 from floodfill import floodFill
-matrix = [[0, 0, 1, 0, 1], [0, 1, 1, 0, 1], [0, 0, 1, 1, 0], [0, 0, 0, 0, 0], [0, 0, 1, 1, 0]]
-y, x, new = 0, 0, 2
+from random import choice
+
+rows, cols = 5, 5
+matrixValues = [0, 1]
+matrix = [[choice(matrixValues) for c in range(cols)] for r in range(rows)]
+
+y, x = 0, 0
+new = 2
+
 result = floodFill(matrix, y, x, new)
 print('Input:', '\n'.join(str(m) for m in matrix), 'Fill from [{0}, {1}]:'.format(y, x), '\n'.join(str(r) for r in result), sep = '\n')
